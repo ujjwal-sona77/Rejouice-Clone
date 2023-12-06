60a18287c05f044c5d0664917cbb64a0d710fa43
Alone_Ujju <146330690+alone-ujju@users.noreply.github.com>
2023-12-06 16:46:07 +0000

    Added some new things and Animations

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ gsap.from(".elem2 h1 span , .elem1 h2 , .elem1 h1" , {
     opacity: 0
 })
 
-gsap.from(".page4 .elem h2" , {
+gsap.from(".page4 .elem h2 , .page4 .elem h1" , {
     scrollTrigger: {
         trigger: ".page4" , 
         scroller: ".main" , 
@@ -92,4 +92,61 @@ gsap.from(".page1_con h1 span" , {
     opacity: 0 ,
     pin: true
 })
+
+function CursorEffectPage4() {
+    var cursor_page4 = document.querySelector(".page4 .animation .cursor");
+    var Page4anima = document.querySelector(".page4 .animation")
+    Page4anima.addEventListener("mousemove", function (dets) {
+        gsap.to(cursor_page4, {
+            x: dets.x,  
+            y: dets.y
+        });
+    })
+
+    Page4anima.addEventListener("mouseenter", function () {
+        gsap.to(cursor, {
+            scale: 1,
+            opacity: 1
+        })
+    })
+
+    Page4anima.addEventListener("mouseleave", function () {
+        gsap.to(cursor, {
+            scale: 0,
+            opacity: 0
+        })
+    })
+}
+
+// CursorEffectPage4();
+
+var animation2_page4 = document.querySelector(".elemm2");
+gsap.from(".elemm2 h1 , .elemm2 h2" , {
+    scrollTrigger: {
+        trigger: ".elemm2" , 
+        scroller: ".main" , 
+        start: "top 47%",
+        end: "top 50%",
+        scrub: 2
+    } ,
+    y: 120 ,
+    stagger: 0.2,
+    duration: 1 ,
+    opacity: 0
+});
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
